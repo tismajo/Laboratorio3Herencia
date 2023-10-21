cadcc903c61dfc1c6083a6d11e405f2f3b9d3bec
María José <118678970+tismajo@users.noreply.github.com>
2023-10-21 14:12:59 -0600

    crearcsv, ingresar nuevo ítem, buscarID

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,3 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Controlador {
+    // Creación del archivo CSV con títulos en la primera fila, por cada columna
+    public void crearVerificarCSV() {
+        File inventarioTienda = new File("inventarioTienda.csv");
+        if (!inventarioTienda.exists()) {
+            try {
+                inventarioTienda.createNewFile();
+                System.out.println("Todo listo para empezar...");
+
+                // Agregar títulos a la primera fila
+                FileWriter fw = new FileWriter(inventarioTienda, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write("id_producto,nombre,cantidad_disponible,cantidad_vendidos,estado,precio,categoria,talla,tipo,color,material");
+                bw.newLine();
+                bw.close();
+
+            } catch (IOException e) {
+                System.out.println("Error encontrado: " + e.getMessage());
+            }
+        }
+    }
+
+    // Se me olvidó ingresar este método en el análisis pero ingresarProductos permitirá ingresar los ítems que desee el usuario.
+    public void ingresarProducto(int id_producto, String nombre, int cantidad_disponible, int cantidad_vendidos, String estado, double precio, String categoria, String talla, String tipo, String color, String material) {
+        String archivo = "inventarioTienda.csv";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo, true))) {
+            writer.write(id_producto + "," + nombre + "," + cantidad_disponible + "," + cantidad_vendidos + "," + estado + "," + precio + "," + categoria + "," + talla + "," + tipo + "," + color + "," + material);
+            writer.newLine();
+            System.out.println(nombre + " agregado con éxito.");
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo CSV: " + e.getMessage());
+        }
+    }    
+
+    public void buscarProducto(int idProducto) {
+        String archivo = "inventarioTienda.csv";
+        String datoBuscado = String.valueOf(idProducto);
     
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(",");
+                if (datos.length > 0 && datos[0].equalsIgnoreCase(datoBuscado)) {
+                    System.out.println(linea);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listarProductos(){
+
+    }
+
+    public void ventasTienda(){
+
+    }
 }
--- src/Vista.java
+++ src/Vista.java
@@ -1,29 +1,66 @@
 import java.util.Scanner;
 
 public class Vista {
-Scanner scn = new Scanner(System.in);
+    Controlador c = new Controlador();
+    Scanner scn = new Scanner(System.in);
 
-public void mostrarMenu() {
-        while (true) {
-            System.out.println("M E N Ú");
-            System.out.println("Ingresa el número de opción: ");
-            System.out.println("[Insertar las opciones que tiene el usuario]");
-            int opcion = scn.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opción inválida. Por favor, ingrese una opción válida :)");
-                    break;
+    public void mostrarMenu() {
+            while (true) {
+                c.crearVerificarCSV();
+                System.out.println("M E N Ú");
+                System.out.println("Ingresa el número de opción: ");
+                System.out.println("1. Ingresar nuevo ítem.\n2. Buscar por ID.");
+                int opcion = scn.nextInt();
+                switch (opcion) {
+                    case 1:
+                    System.out.println("Ingresa la información que se le solicita a continuación acerca del producto:");
+                    System.out.print("Ingresa el id: ");
+                    int id_producto = scn.nextInt();
+                    scn = new Scanner(System.in);
+                    System.out.print("Ingresa el nombre: ");
+                    String nombre = scn.nextLine();
+                    System.out.print("Ingresa la cantidad: ");
+                    int cantidad_disponible = scn.nextInt();
+                    System.out.print("Ingresa la cantidad vendida: ");
+                    int cantidad_vendidos = scn.nextInt();
+                    scn = new Scanner(System.in);
+                    System.out.print("Ingresa el estado (disponible, reservado, vendido): ");
+                    String estado = scn.nextLine();
+                    System.out.print("Ingresa el precio: Q");
+                    double precio = scn.nextDouble();
+                    scn = new Scanner(System.in);
+                    System.out.print("Ingresa la categoría (Pantalones, blusa o camisa, collar): ");
+                    String categoria = scn.nextLine();
+                    System.out.print("Ingresa la talla: ");
+                    String talla = scn.nextLine();
+                    if (categoria.equalsIgnoreCase("pantalones")){
+                        System.out.print("Ingresa el tipo (lona, jogger, pants...etc): ");
+                        String tipo = scn.nextLine();
+                        c.ingresarProducto(id_producto, nombre, cantidad_disponible, cantidad_vendidos, estado, precio, categoria, talla, tipo, null, null);
+                    } else if (categoria.equalsIgnoreCase("blusa") || categoria.equalsIgnoreCase("camisa")) {
+                        System.out.print("Ingresa el color: ");
+                        String color = scn.nextLine();
+                        c.ingresarProducto(id_producto, nombre, cantidad_disponible, cantidad_vendidos, estado, precio, categoria, talla, null, color, null);
+                    } else if (categoria.equalsIgnoreCase("collar")){
+                        System.out.print("Ingresa el material: ");
+                        String material = scn.nextLine();
+                        c.ingresarProducto(id_producto, nombre, cantidad_disponible, cantidad_vendidos, estado, precio, categoria, talla, null, null, material);
+                    }
+                        break;
+                    case 2:
+                    System.out.print("Ingresa el id del producto que deseas buscar: ");
+                    int idProducto = scn.nextInt();
+                    c.buscarProducto(idProducto);
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Opción inválida. Por favor, Ingresa una opción válida :)");
+                        break;
+                }
             }
-        }
-}
+    }
 }
